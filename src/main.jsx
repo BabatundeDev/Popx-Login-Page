ceed86cf27323d79c72c5913f1c2098a79b25de3
Babatunde Ishau <ishautunde700@gmail.com>
2025-05-27 21:30:20 +0100

    Fix: Set basename for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Popx-Login-Page">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
