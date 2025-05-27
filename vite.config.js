d3497913208d2d1c4333efd5698f31b44385dada
Babatunde Ishau <ishautunde700@gmail.com>
2025-05-27 21:58:33 +0100

    Fix: update base path and add 404 redirect for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'Popx-Login-Page',
+  base: '/Popx-Login-Page/',
   plugins: [react()],
 })
