7c8d896523b8b8da1f337c5b0fd8cd04669516f8
Babatunde Ishau <ishautunde700@gmail.com>
2025-05-28 11:14:31 +0100

    Fix vite base config for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Popx-Login-Page/',
+  // base: '/Popx-Login-Page/',
   plugins: [react()],
 })
